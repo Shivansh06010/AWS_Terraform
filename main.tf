d850e923079400f9df40737f6ef26e45580a60a5
Shivansh <shivanshsharma06010@gmail.com>
2026-09-12 21:25:55 +0530

    created an ec2

--- main.tf
+++ main.tf
@@ -10,9 +10,21 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+resource "ec2_instance" "test instance" {
+  ami           = "ami-08188a5a4dfdbd573"
+  instance_type = "t2.micro"
+  key_name = "test key"
+  vpc_id = "vpc-04601c00877beba1b"
+  subnet_id = "subnet-022001eec6e4fbe4b"
+  security_groups = ["sg-05e63dc54f3efed5e"]
+
+  tags = {
+    Name = "Managed by terraform"
+  }
+}
 
 resource "aws_s3_bucket" "example" {
-  bucket = "shivansh-s3-bucket-06010"
+  bucket = var.s3_bucket_name
 
   tags = {
     Name        = "Managed by terraform"
